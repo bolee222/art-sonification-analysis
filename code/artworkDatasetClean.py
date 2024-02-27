c5c1dc6de1a6d0391520a3f19800a3bb709fddb7
bolee <30682567+bolee222@users.noreply.github.com>
2024-02-28 02:29:18 +0900

    dataset cleanup

---
+++ code/artworkDatasetClean.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme()
+
+def transformCSV(filePath):
+    #'../dataset/artworks/A1-Table 1.csv'
+    artworkCode = filePath.split("/")[3][0:2]
+    print(artworkCode)
+
+    df = pd.read_csv(filePath)
+    print(df)
+
+    c_name = df.columns[0] #nickname
+    c_sod = df.columns[1] #sod
+    c_gRow1 = df.columns[2] #grid_row1
+    c_gRow2 = df.columns[3] #grid_row2
+    c_gRow3 = df.columns[4] #grid_row3
+    c_gRow4 = df.columns[5] #grid_row4
+    c_reasons = df.columns[6] #grid_row4
+    c_tempo = df.columns[7] #tempo
+    c_pitch = df.columns[8] #pitch
+    c_density = df.columns[9] #density
+    c_others = df.columns[10] #others
+
+    df.rename(columns = {c_name : "name", c_sod : "sod", c_gRow1 : "gRoq1", c_gRow2 : "gRoq2", c_gRow3 : "gRoq3", c_gRow4 : "gRoq4", c_reasons : "reasons", c_tempo : "tempo", c_pitch : "pitch", c_density : "density", c_others : "others"}, inplace = True)
+
+    df["tempo_score"] = df["tempo"].str[0].astype(int)
+    df["pitch_score"] = df["pitch"].str[0].astype(int)
+    df["density_score"] = df["density"].str[0].astype(int)
+    df["artwork"] = artworkCode
+
+    df=df[["name", "artwork", "sod","gRoq1", "gRoq2", "gRoq3", "gRoq4", "reasons", "tempo_score", "tempo", "pitch_score","pitch", "density_score", "density"]]
+    
+    df.to_csv('../dataset_cleanUp/' + artworkCode + '.csv')
+
+transformCSV('../dataset/artworks/A1-Table 1.csv')
